9cf649a0580d5e7327416a74a8acb9425e77ee04
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-09-05 22:41:22 -0400

    mapped id to photo

--- lib/vision_server_web/resolvers/posts.ex
+++ lib/vision_server_web/resolvers/posts.ex
@@ -2,6 +2,10 @@ defmodule VisionServerWeb.Resolvers.Posts do
   def photos(_, _, _) do
     {:ok, VisionServer.Posts.list_photos()}
   end
+
+  def photo(_, %{id: id}, _) do
+    {:ok, VisionServer.Posts.get_photo!(id)}
+  end
 end
 
 # resolver accessing list photos
